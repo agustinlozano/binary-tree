0348ff20c8ef94a3fc11f012b7e2ac6e08960cef
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-12-03 14:40:41 -0300

    Edit addnode file

--- tree/addnode.c
+++ tree/addnode.c
@@ -1,26 +1,76 @@
 #include "tree.h"
 
+/*  Documentation:
+  Parametros:
+    n     es cualquier nodo
+    data  es el dato de un nodo que aun no existe
+
+  La funcion es recursiva por tanto el codigo
+    if n igual null es caso base
+  Que seria cuando llego a una hoja en el arbol
+
+  Por otra parte, si se da que
+    n es distinto de null
+  entonces la funcion se llama recursivamente
+
+  En definitiva la funcion solo agrega un nodo
+  en el brazo del arbol que corresponda.
+*/
+
 void addNewNode(Node **n, char *data) {
   Node *newNode = (Node *)malloc(sizeof(Node));
+  Node *aux;
+  int cmpValue;
 
   if (*n == NULL) {
-    printf("\nSe agrega una hoja del arbol");
-
     (*n) = newNode;
     setLeaves(*n);
     setData(*n, data);
 
   } else {
-    int value = strcmp(data, (*n)->data);
+    //Comparar data contra el root
+    if ((*n)->left == NULL && (*n)->right == NULL) {
+      cmpValue = strcmp(data,(*n)->data);
+      
+      if (cmpValue <= 0) {
+        addNewNode(&((*n)->left), data);
 
-    if (value <= 1) {
-      addNewNode(&((*n)->left), data);
+      } else {
+        addNewNode(&((*n)->right), data);
+      }
 
-    } else if (value < 1) {
-      addNewNode(&((*n)->right), data);
+    //comparar data contra el contendido del nodo left
+    } else if ((*n)->left != NULL && (*n)->right == NULL) {
+      cmpValue = strcmp(data,(*n)->left->data);
+
+      if (cmpValue <= 0) {
+        aux = (*n)->left;
+        (*n) -> left = NULL;
+
+        addNewNode(&((*n)->left), data);
+        addNewNode(&((*n)->right), aux->data);
+
+      } else {
+        addNewNode(&((*n)->right), data);
+      }
+    
+    //comparar data contra el contendido del nodo right
+    } else if ((*n)->left == NULL && (*n)->right != NULL) {
+      cmpValue = strcmp(data,(*n)->right->data);
+
+      if (cmpValue <= 0) {
+        addNewNode(&((*n)->left), data);
+
+      } else {
+        aux = (*n)->right;
+        (*n)->right = NULL;
+
+        addNewNode(&((*n)->right), data);
+        addNewNode(&((*n)->left), aux->data);
+      }
 
     } else {
-      printf("\nError: algo salio mal!\n");
+      printf("\nUps! Parece que hubo un error\n");
     }
   }
 }
